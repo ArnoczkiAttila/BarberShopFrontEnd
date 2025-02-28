b04a87b28bc6b8a0489aac403e38e374256f1b7f
ArnoczkiAttila <77162976+ArnoczkiAttila@users.noreply.github.com>
2025-02-28 09:11:16 +0100

    setup

---
+++ src/components/Barber.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import { ref } from 'vue';
+    const barber = ref({
+        'barber_name':''
+    });
+    
+    const uploadBarber = async () => {
+
+    }
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
